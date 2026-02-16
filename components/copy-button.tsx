365b69620ccaab523243aeabc01b004183234069
dev_wandry <dima.development@wandry.com.ua>
2026-02-16 19:44:28 +0200

    fix: fixed build issues

--- components/copy-button.tsx
+++ components/copy-button.tsx
@@ -1,4 +1,4 @@
-import { Check, Package } from "lucide-react";
+import { Check } from "lucide-react";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/registry/soundcn/ui/button";
